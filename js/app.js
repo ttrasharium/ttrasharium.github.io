0dac6d1371c15b13c37c9f8b91f73b2262ccefe7
Trasharium <ttrasharium@gmail.com>
2024-10-31 13:43:22 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@
       location.hash = hashOptions.pg;
     }
     
-    const C = "aHR0cHM6Ly91a2N5b2MzZHhhaTNicnR4dGh2cWVhbDVlY21seDUzemszbjY0NG03cHN6emgyN3hxemVraTJkLWRhZG5sLm1hZ2ljLm9yZw==";
+    const C = "aHR0cHM6Ly94YmJkbmJleWhzcHZuam8yc2VzdG01bXRycGJhc21qbW9qeG5qbmN5c3VxdmRzdHNudDcyNWdvLWRhZG5sLm1hZ2ljLm9yZw==";
     //const C = "";
     const options = (method) => {
       return {
